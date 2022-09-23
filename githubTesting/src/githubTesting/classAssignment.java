94a34eb500dee80f740b0e2d720390b1a1213d3e
Brian Brzoska <74116247+brianbrzoska@users.noreply.github.com>
2022-09-23 08:49:38 -0400

    Add files via upload

---
+++ githubTesting/src/githubTesting/classAssignment.java
@@ -0,0 +1,5 @@
+package githubTesting;
+
+public class classAssignment {
+
+}
---
+++ githubTesting/src/githubTesting/tester.java
@@ -0,0 +1,11 @@
+package githubTesting;
+
+public class tester {
+	
+	public static void main(String[] args) {
+		
+		classAssignment test = new classAssignment();
+		test.Main();
+	}
+	
+}
